2f2e175d37f664225a14076ed689ec48d3ee6479
Michael Ly <michael.ly2839@gmail.com>
2022-10-18 12:02:27 -0400

    globals

--- 3D Scanner/ViewController.swift
+++ 3D Scanner/ViewController.swift
@@ -6,12 +6,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    // Things to do:
+    
+    // Capture Session
+    var session: AVCaptureSession?
+    // Photo Output
+    let output = AVCapturePhotoOutput()
+    // Video Preview
+    let previewLayer = AVCaptureVideoPreviewLayer()
+    // Shutter Button
+    private let shutterButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        button.layer.cornerRadius = 100
+        button.layer.borderWidth = 10
+        button.layer.borderColor = UIColor.white.cgColor
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .black
     }
 
 
